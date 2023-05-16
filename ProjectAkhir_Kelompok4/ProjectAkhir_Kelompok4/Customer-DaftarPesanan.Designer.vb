52a694e13bc6b4721ef1a559d09735d83ccb538c
Natalieefd <nataliefuad2003@gmail.com>
2023-05-16 14:57:58 +0800

    up

---
+++ ProjectAkhir_Kelompok4/ProjectAkhir_Kelompok4/Customer-DaftarPesanan.Designer.vb
@@ -0,0 +1,246 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class DaftarPesananCustomer
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim DataGridViewCellStyle1 As System.Windows.Forms.DataGridViewCellStyle = New System.Windows.Forms.DataGridViewCellStyle()
+        Dim DataGridViewCellStyle2 As System.Windows.Forms.DataGridViewCellStyle = New System.Windows.Forms.DataGridViewCellStyle()
+        Dim DataGridViewCellStyle3 As System.Windows.Forms.DataGridViewCellStyle = New System.Windows.Forms.DataGridViewCellStyle()
+        Dim DataGridViewCellStyle4 As System.Windows.Forms.DataGridViewCellStyle = New System.Windows.Forms.DataGridViewCellStyle()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.StatusStrip1 = New System.Windows.Forms.StatusStrip()
+        Me.SlabelTanggal = New System.Windows.Forms.ToolStripStatusLabel()
+        Me.SlabelJam = New System.Windows.Forms.ToolStripStatusLabel()
+        Me.MenuStrip1 = New System.Windows.Forms.MenuStrip()
+        Me.ProfilTokoToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.KatalogToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.PesananToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.LihatPesananToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.BuatPesananToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.UbahPesananToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.HapusPesananToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ProfilToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.LihatPesananToolStripMenuItem1 = New System.Windows.Forms.ToolStripMenuItem()
+        Me.UbahPesananToolStripMenuItem1 = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ExitToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.dgvDaftarPesanan = New System.Windows.Forms.DataGridView()
+        Me.StatusStrip1.SuspendLayout()
+        Me.MenuStrip1.SuspendLayout()
+        CType(Me.dgvDaftarPesanan, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Timer1
+        '
+        '
+        'StatusStrip1
+        '
+        Me.StatusStrip1.Font = New System.Drawing.Font("Vela Sans GX", 8.999999!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.StatusStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.SlabelTanggal, Me.SlabelJam})
+        Me.StatusStrip1.Location = New System.Drawing.Point(0, 428)
+        Me.StatusStrip1.Name = "StatusStrip1"
+        Me.StatusStrip1.Size = New System.Drawing.Size(800, 22)
+        Me.StatusStrip1.TabIndex = 8
+        Me.StatusStrip1.Text = "StatusStrip1"
+        '
+        'SlabelTanggal
+        '
+        Me.SlabelTanggal.Name = "SlabelTanggal"
+        Me.SlabelTanggal.Size = New System.Drawing.Size(59, 17)
+        Me.SlabelTanggal.Text = "Tanggal : "
+        '
+        'SlabelJam
+        '
+        Me.SlabelJam.Name = "SlabelJam"
+        Me.SlabelJam.RightToLeft = System.Windows.Forms.RightToLeft.No
+        Me.SlabelJam.Size = New System.Drawing.Size(39, 17)
+        Me.SlabelJam.Text = "Jam : "
+        '
+        'MenuStrip1
+        '
+        Me.MenuStrip1.Font = New System.Drawing.Font("Vela Sans GX", 8.999999!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.MenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.ProfilTokoToolStripMenuItem, Me.KatalogToolStripMenuItem, Me.PesananToolStripMenuItem, Me.ProfilToolStripMenuItem, Me.ExitToolStripMenuItem})
+        Me.MenuStrip1.Location = New System.Drawing.Point(0, 0)
+        Me.MenuStrip1.Name = "MenuStrip1"
+        Me.MenuStrip1.Size = New System.Drawing.Size(800, 25)
+        Me.MenuStrip1.TabIndex = 7
+        Me.MenuStrip1.Text = "MenuStrip1"
+        '
+        'ProfilTokoToolStripMenuItem
+        '
+        Me.ProfilTokoToolStripMenuItem.Name = "ProfilTokoToolStripMenuItem"
+        Me.ProfilTokoToolStripMenuItem.Size = New System.Drawing.Size(75, 21)
+        Me.ProfilTokoToolStripMenuItem.Text = "Profil Toko"
+        '
+        'KatalogToolStripMenuItem
+        '
+        Me.KatalogToolStripMenuItem.Name = "KatalogToolStripMenuItem"
+        Me.KatalogToolStripMenuItem.Size = New System.Drawing.Size(63, 21)
+        Me.KatalogToolStripMenuItem.Text = "Katalog"
+        '
+        'PesananToolStripMenuItem
+        '
+        Me.PesananToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.LihatPesananToolStripMenuItem, Me.BuatPesananToolStripMenuItem, Me.UbahPesananToolStripMenuItem, Me.HapusPesananToolStripMenuItem})
+        Me.PesananToolStripMenuItem.Name = "PesananToolStripMenuItem"
+        Me.PesananToolStripMenuItem.Size = New System.Drawing.Size(68, 21)
+        Me.PesananToolStripMenuItem.Text = "Pesanan"
+        '
+        'LihatPesananToolStripMenuItem
+        '
+        Me.LihatPesananToolStripMenuItem.Name = "LihatPesananToolStripMenuItem"
+        Me.LihatPesananToolStripMenuItem.Size = New System.Drawing.Size(161, 22)
+        Me.LihatPesananToolStripMenuItem.Text = "Lihat Pesanan"
+        '
+        'BuatPesananToolStripMenuItem
+        '
+        Me.BuatPesananToolStripMenuItem.Name = "BuatPesananToolStripMenuItem"
+        Me.BuatPesananToolStripMenuItem.Size = New System.Drawing.Size(161, 22)
+        Me.BuatPesananToolStripMenuItem.Text = "Buat Pesanan"
+        '
+        'UbahPesananToolStripMenuItem
+        '
+        Me.UbahPesananToolStripMenuItem.Name = "UbahPesananToolStripMenuItem"
+        Me.UbahPesananToolStripMenuItem.Size = New System.Drawing.Size(161, 22)
+        Me.UbahPesananToolStripMenuItem.Text = "Ubah Pesanan"
+        '
+        'HapusPesananToolStripMenuItem
+        '
+        Me.HapusPesananToolStripMenuItem.Name = "HapusPesananToolStripMenuItem"
+        Me.HapusPesananToolStripMenuItem.Size = New System.Drawing.Size(161, 22)
+        Me.HapusPesananToolStripMenuItem.Text = "Hapus Pesanan"
+        '
+        'ProfilToolStripMenuItem
+        '
+        Me.ProfilToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.LihatPesananToolStripMenuItem1, Me.UbahPesananToolStripMenuItem1})
+        Me.ProfilToolStripMenuItem.Name = "ProfilToolStripMenuItem"
+        Me.ProfilToolStripMenuItem.Size = New System.Drawing.Size(48, 21)
+        Me.ProfilToolStripMenuItem.Text = "Profil"
+        '
+        'LihatPesananToolStripMenuItem1
+        '
+        Me.LihatPesananToolStripMenuItem1.Name = "LihatPesananToolStripMenuItem1"
+        Me.LihatPesananToolStripMenuItem1.Size = New System.Drawing.Size(155, 22)
+        Me.LihatPesananToolStripMenuItem1.Text = "Lihat Pesanan"
+        '
+        'UbahPesananToolStripMenuItem1
+        '
+        Me.UbahPesananToolStripMenuItem1.Name = "UbahPesananToolStripMenuItem1"
+        Me.UbahPesananToolStripMenuItem1.Size = New System.Drawing.Size(155, 22)
+        Me.UbahPesananToolStripMenuItem1.Text = "Ubah Pesanan"
+        '
+        'ExitToolStripMenuItem
+        '
+        Me.ExitToolStripMenuItem.Name = "ExitToolStripMenuItem"
+        Me.ExitToolStripMenuItem.Size = New System.Drawing.Size(41, 21)
+        Me.ExitToolStripMenuItem.Text = "Exit"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("Vela Sans GX", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.Location = New System.Drawing.Point(299, 65)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(193, 28)
+        Me.Label4.TabIndex = 15
+        Me.Label4.Text = "DAFTAR PESANAN"
+        '
+        'dgvDaftarPesanan
+        '
+        Me.dgvDaftarPesanan.BackgroundColor = System.Drawing.SystemColors.ActiveBorder
+        DataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter
+        DataGridViewCellStyle1.BackColor = System.Drawing.SystemColors.Control
+        DataGridViewCellStyle1.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        DataGridViewCellStyle1.ForeColor = System.Drawing.SystemColors.WindowText
+        DataGridViewCellStyle1.SelectionBackColor = System.Drawing.SystemColors.Highlight
+        DataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.HighlightText
+        DataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.[True]
+        Me.dgvDaftarPesanan.ColumnHeadersDefaultCellStyle = DataGridViewCellStyle1
+        Me.dgvDaftarPesanan.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        DataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter
+        DataGridViewCellStyle2.BackColor = System.Drawing.SystemColors.Window
+        DataGridViewCellStyle2.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        DataGridViewCellStyle2.ForeColor = System.Drawing.SystemColors.ControlText
+        DataGridViewCellStyle2.SelectionBackColor = System.Drawing.SystemColors.Highlight
+        DataGridViewCellStyle2.SelectionForeColor = System.Drawing.SystemColors.HighlightText
+        DataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.[False]
+        Me.dgvDaftarPesanan.DefaultCellStyle = DataGridViewCellStyle2
+        Me.dgvDaftarPesanan.Location = New System.Drawing.Point(57, 135)
+        Me.dgvDaftarPesanan.Name = "dgvDaftarPesanan"
+        DataGridViewCellStyle3.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter
+        DataGridViewCellStyle3.BackColor = System.Drawing.SystemColors.Control
+        DataGridViewCellStyle3.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        DataGridViewCellStyle3.ForeColor = System.Drawing.SystemColors.WindowText
+        DataGridViewCellStyle3.SelectionBackColor = System.Drawing.SystemColors.Highlight
+        DataGridViewCellStyle3.SelectionForeColor = System.Drawing.SystemColors.HighlightText
+        DataGridViewCellStyle3.WrapMode = System.Windows.Forms.DataGridViewTriState.[True]
+        Me.dgvDaftarPesanan.RowHeadersDefaultCellStyle = DataGridViewCellStyle3
+        DataGridViewCellStyle4.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter
+        Me.dgvDaftarPesanan.RowsDefaultCellStyle = DataGridViewCellStyle4
+        Me.dgvDaftarPesanan.Size = New System.Drawing.Size(686, 251)
+        Me.dgvDaftarPesanan.TabIndex = 14
+        '
+        'DaftarPesananCustomer
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.AutoScroll = True
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.dgvDaftarPesanan)
+        Me.Controls.Add(Me.StatusStrip1)
+        Me.Controls.Add(Me.MenuStrip1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D
+        Me.MaximizeBox = False
+        Me.MinimizeBox = False
+        Me.Name = "DaftarPesananCustomer"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "KPOP Store"
+        Me.StatusStrip1.ResumeLayout(False)
+        Me.StatusStrip1.PerformLayout()
+        Me.MenuStrip1.ResumeLayout(False)
+        Me.MenuStrip1.PerformLayout()
+        CType(Me.dgvDaftarPesanan, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Timer1 As Timer
+    Friend WithEvents StatusStrip1 As StatusStrip
+    Friend WithEvents SlabelTanggal As ToolStripStatusLabel
+    Friend WithEvents SlabelJam As ToolStripStatusLabel
+    Friend WithEvents MenuStrip1 As MenuStrip
+    Friend WithEvents ProfilTokoToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents KatalogToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents PesananToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents LihatPesananToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents BuatPesananToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents UbahPesananToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents HapusPesananToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents ProfilToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents LihatPesananToolStripMenuItem1 As ToolStripMenuItem
+    Friend WithEvents UbahPesananToolStripMenuItem1 As ToolStripMenuItem
+    Friend WithEvents ExitToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents Label4 As Label
+    Friend WithEvents dgvDaftarPesanan As DataGridView
+End Class
---
+++ ProjectAkhir_Kelompok4/ProjectAkhir_Kelompok4/Customer-DaftarPesanan.vb
@@ -0,0 +1,9 @@
+﻿Public Class DaftarPesananCustomer
+    Private Sub DaftarPesananCustomer_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        SlabelTanggal.Text = Today
+    End Sub
+
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        SlabelJam.Text = TimeOfDay
+    End Sub
+End Class
