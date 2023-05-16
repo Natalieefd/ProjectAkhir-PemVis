e20bc82f72fcb2c66c6f38fa65b2582d4a673be5
agustinadwimaharani <93140300+agustinadwimaharani@users.noreply.github.com>
2023-05-16 10:54:22 +0800

    module

---
+++ ProjectAkhir_Kelompok4/ProjectAkhir_Kelompok4/Database.vb
@@ -0,0 +1,23 @@
+﻿Imports System.Data.SqlClient
+Imports MySql.Data.MySqlClient
+Module Database
+    Public CONN As MySqlConnection
+    Public CMD As MySqlCommand
+    Public RD As MySqlDataReader
+    Public DA As MySqlDataAdapter
+    Public DS As DataSet
+    Public STR As DataSet
+
+    Sub koneksi()
+        Try
+            Dim STR As String = "server=localhost;userid=root;password=;database=dbkpopstore"
+            CONN = New MySqlConnection(STR)
+            If CONN.State = ConnectionState.Closed Then
+                CONN.Open()
+            End If
+        Catch ex As Exception
+            MessageBox.Show(ex.Message)
+        End Try
+    End Sub
+End Module
+
