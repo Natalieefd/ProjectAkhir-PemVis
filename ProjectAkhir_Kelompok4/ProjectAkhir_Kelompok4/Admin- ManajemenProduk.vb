e821c26ff1dbf84a8f8b92cb0978d9ca59443fe9
Natalieefd <nataliefuad2003@gmail.com>
2023-05-18 16:07:12 +0800

    Update ManajemenProduk Admin

--- ProjectAkhir_Kelompok4/ProjectAkhir_Kelompok4/Admin- ManajemenProduk.vb
+++ ProjectAkhir_Kelompok4/ProjectAkhir_Kelompok4/Admin- ManajemenProduk.vb
@@ -1,11 +1,64 @@
 ﻿Public Class ManajemenProdukAdmin
     Private Sub ManajemenProdukAdmin_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         SlabelTanggal.Text = Today
-    End Sub
-
-    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
         SlabelJam.Text = TimeOfDay
     End Sub
+
+    Private Sub HomeToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles HomeToolStripMenuItem.Click
+        formAdmin.Show()
+    End Sub
+
+    Private Sub ProfilTokoToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles ProfilTokoToolStripMenuItem.Click
+
+    End Sub
+
+    Private Sub LihatAkunStaffToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles LihatAkunStaffToolStripMenuItem.Click
+
+    End Sub
+
+    Private Sub TambahAkunStaffToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles TambahAkunStaffToolStripMenuItem.Click
+
+    End Sub
+
+    Private Sub UbahAkunStaffToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles UbahAkunStaffToolStripMenuItem.Click
+
+    End Sub
+
+    Private Sub DeleteAkunStaffToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles DeleteAkunStaffToolStripMenuItem.Click
+
+    End Sub
+
+    Private Sub LihatDataProdukToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles LihatDataProdukToolStripMenuItem.Click
+        Me.Show()
+    End Sub
+
+    Private Sub TambahDataProdukToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles TambahDataProdukToolStripMenuItem.Click
+        Me.Show()
+    End Sub
+
+    Private Sub UbahDataProdukToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles UbahDataProdukToolStripMenuItem.Click
+        Me.Show()
+    End Sub
+
+    Private Sub HapusDataProdukToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles HapusDataProdukToolStripMenuItem.Click
+        Me.Show()
+    End Sub
+
+    Private Sub PesananToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles PesananToolStripMenuItem.Click
+        DaftarPesananAdmin.Show()
+        Me.Hide()
+    End Sub
+
+    Private Sub ExitToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles ExitToolStripMenuItem.Click
+        Me.ActiveControl = MenuStrip1
+        Home.Show()
+        Me.Close()
+    End Sub
+
+    Private Sub ManajemenProdukAdmin_FormClosing(sender As Object, e As FormClosingEventArgs) Handles MyBase.FormClosing
+        CloseForm(sender, MenuStrip1)
+    End Sub
+
     Private Sub btnClose_Click(sender As Object, e As EventArgs) Handles btnClose.Click
         Me.Close()
     End Sub
